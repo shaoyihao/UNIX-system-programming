fc1f2012c967f01f084fb645f35cc701628cecbb
shaoyihao <shaoyihao0517@qq.com>
2022-12-16 13:59:59 +0800

    updata more1.c

--- more1.c
+++ more1.c
@@ -3,49 +3,51 @@
 #define PAGELEN 24
 #define LINELEN 512
 
-int see_more()
+int print_X_line()  //判断是否需要继续输出，返回需要再输出的行数
 {
-    int c;
     printf("\033[7m more? \033[m");
+
+    int c;
     while ((c = getchar()) != EOF)
     {
-        if (c == 'q') return 0;
-        if (c == ' ') return PAGELEN;
-        if (c == '\n') return 1;
+        if (c == 'q') return 0;       //再输出0行（即退出）
+        if (c == ' ') return PAGELEN; //再输出PAGELEN行（即输出一屏）
+        if (c == '\n') return 1;      //再输出一行
     }
     return 0;
 }
 
-void do_more(FILE* fp)
+void display(FILE* fp)  //输出fp中的内容
 {
+    int num_of_line = 0; //当前显示的行数（主要考虑 num_of_line 与 PAGELEN 的差值）
+
     char line[LINELEN];
-    int num_of_line = 0, reply;
-    while (fgets(line, LINELEN, fp))
+    while (fgets(line, LINELEN, fp))  //从fp中读取数据存入line中
     {
-        if (num_of_line == PAGELEN)
+        if (num_of_line == PAGELEN) // 显示一屏后
         {
-            reply = see_more();
-            if (reply == 0) break;
-            num_of_line -= reply;
+            int t = print_X_line();
+
+            if (t == 0) break;
+            else num_of_line -= t;
         }
-        if (fputs(line, stdout) == EOF) exit(1);
+        if (fputs(line, stdout) == EOF) exit(1); //将line中的数据输出到stdout中
         num_of_line ++;
     }
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc == 1) do_more(stdin);
+    if (argc == 1) display(stdin);
     else 
     {
         FILE* fp;
-        for (int i = 1; i < argc; i ++)
-            if ((fp = fopen(argv[i], "r")) != NULL)
-            {
-                do_more(fp);
-                fclose(fp);
-            }
-            else exit(1);
+        for (int i = 1; i < argc; i ++)  //枚举命令行给出的各个文件名
+        {
+            if ((fp = fopen(argv[i], "r")) == NULL) exit(1);
+            display(fp);
+            fclose(fp);
+        }
     }
     return 0;
 }
