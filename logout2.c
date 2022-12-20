800bec52d75907d2def791602702a1547a521f87
shaoyihao <shaoyihao0517@qq.com>
2022-12-20 21:11:47 +0800

    rename logout2.c

--- logout2.c
+++ logout2.c
@@ -11,15 +11,14 @@ void logout_tty(char* line)
     int fd = open(_PATH_UTMP, O_RDONLY);
     
     struct utmp rec;
-    while (read(fd, &rec, UTSIZE) == UTSIZE)
+    while (read(fd, &rec, UTSIZE) == UTSIZE) //枚举每条记录，查找指定终端名
         if (strncmp(rec.ut_line, line, sizeof(rec.ut_line) == 0))
         {
             rec.ut_type = DEAD_PROCESS;
-            time(&rec.ut_time);
-            lseek(fd, -UTSIZE, SEEK_CUR); //调整指针位置
-            write(fd, &rec, UTSIZE);
-            // close(fd);
-            // return;
+            time(&rec.ut_time); //修改属性
+
+            lseek(fd, -UTSIZE, SEEK_CUR); //调整指针到本条记录的首部
+            write(fd, &rec, UTSIZE); //重写本记录
         }
 
     close(fd);
